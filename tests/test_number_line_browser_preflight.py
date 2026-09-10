e93648694e8ec9b16131ce72024727166dc43924
Haitham Hamadneh <haitham.haj@gmail.com>
2026-09-10 13:28:22 +0300

    test: advance browser preflight schema head

--- tests/test_number_line_browser_preflight.py
+++ tests/test_number_line_browser_preflight.py
@@ -46,7 +46,7 @@ def test_preflight_returns_sanitized_identity_only_for_the_expected_target():
 
 
 def test_preflight_reads_the_repository_migration_head():
-    assert setup_module().expected_schema_version() == '8f4d2a9c6b31'
+    assert setup_module().expected_schema_version() == '9b6e2c4d8f01'
 
 
 def test_preflight_accepts_the_database_row_mapping_returned_by_sqlalchemy():
